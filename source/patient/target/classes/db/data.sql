31a74af3f96dae39ad4586bfed41d767f80a70e3
Konur <konur.unyelioglu@icare.com>
2016-10-30 18:06:02 -0400

    Deleting contents from target directories.

--- source/patient/target/classes/db/data.sql
+++
@@ -1,8 +0,0 @@
-insert into PATIENT (NAME, NUMBER) values ('Frank Haines', 1001);
-insert into PATIENT (NAME, NUMBER) values ('John Doe', 1002);
-insert into PATIENT (NAME, NUMBER) values ('Mary Haines', 1003);
-insert into PATIENT (NAME, NUMBER, SYSP) values ('Jane Doe', 1004, 130);
-insert into PATIENT (NAME, NUMBER) values ('Mary Jones', 1005);
-insert into PATIENT (NAME, NUMBER) values ('Henry Jones', 1006);
-
-
